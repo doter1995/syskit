cd51f91ecfddcc5296a961197266715a0297863a
weiDong ZHang <doter1995@qq.com>
2019-02-22 00:12:33 +0800

    add jq test

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const { app, BrowserWindow } = require("electron");
+const os = require("os");
 function createWindow() {
-  let win = new BrowserWindow({ width: 800, height: 600 });
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+  global.sharedObject = {
+    someProperty: "default value"
+  };
   win.loadFile("index.html");
+  console.log(os.type());
+  // 在主进程中
+  win.webContents.toggleDevTools();
 }
 app.on("ready", createWindow);
